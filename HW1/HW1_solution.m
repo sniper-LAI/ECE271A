3b119ea10266768b74912173a64233437a67c82f
sniper-LAI <445124369@qq.com>
2019-10-10 22:14:59 -0700

    First solution

--- HW1/HW1_solution.m
+++ HW1/HW1_solution.m
@@ -1,4 +1,5 @@
 clear all
+format long
 %%
 %Training
 %Read the TrainingSamplesDCT_8.mat file
@@ -9,13 +10,21 @@ train_FG = TrainsampleDCT_FG;
 
 %Find the second largest value in each row of matrix train_BG
 [M_BG,N_BG] = max(train_BG,[],2);
-train_BG(bsxfun(@eq, train_BG, M_BG)) = -Inf; % Set the largest value in each row to -inf
+train_BG(bsxfun(@eq, train_BG, M_BG)) = -1; % Set the largest value in each row to -inf
 [M_BG,N_BG] = max(train_BG,[],2);
 
 %Find the second largest value in each row of matrix train_FG
 [M_FG,N_FG] = max(train_FG,[],2);
-train_FG(bsxfun(@eq, train_FG, M_FG)) = -Inf; % Set the largest value in each row to -inF
+train_FG(bsxfun(@eq, train_FG, M_FG)) = -1; % Set the largest value in each row to -inF
 [M_FG,N_FG] = max(train_FG,[],2);
+
+% [M_BG,N_BG] = find(train_BG==max(train_BG(:))); % Find the largest coefficient
+% train_BG(M_BG,N_BG) = -Inf; % Set the largest value as -Inf
+% [M_BG,N_BG] = find(train_BG==max(train_BG(:))); % Find the second largest coefficient and its position
+% 
+% [M_FG,N_FG] = find(train_FG==max(train_FG(:))); % Find the largest coefficient
+% train_FG(M_BG,N_BG) = -Inf; % Set the largest value as -Inf
+% [M_FG,N_FG] = find(train_FG==max(train_FG(:))); % Find the second largest coefficient and its position
 
 %Plot the histogram
 subplot(2,1,1);
@@ -31,9 +40,9 @@ ylabel('Frequency', 'interpreter', 'latex','FontSize', 10);
 xlabel('Index', 'interpreter', 'latex'); 
 title({['Index histograms of $$P_{X|Y}(x|grass)$$']},'Fontsize',12,'interpreter','latex');
 %Save the statistic data
-F_x_BG = zeros(1,64)
+F_x_BG = zeros(1,64);
 F_x_BG(min(N_BG):max(N_BG)) = h1.Values;
-F_x_FG = zeros(1,64)
+F_x_FG = zeros(1,64);
 F_x_FG(min(N_FG):max(N_FG)) = h2.Values;
 %Save the histogram figure
 set(gcf,'Position',[400,100,900,600]);
@@ -45,7 +54,39 @@ P_x_BG = F_x_BG ./ sum(F_x_BG);
 P_x_FG = F_x_FG ./ sum(F_x_FG);
 P_BG = size(train_BG,1) / (size(train_BG,1) + size(train_FG,1));
 P_FG = size(train_FG,1) / (size(train_BG,1) + size(train_FG,1));
-%%
+
 %Read original image
 I = imread('dataset/cheetah.bmp');
 I = im2double(I);
+%Define the loop numbers
+loop_row = size(I,1) - 8 + 1;
+loop_column = size(I,2) - 8 + 1;
+
+mask = zeros(size(I));
+position_ref = load('dataset/Zig-Zag Pattern.txt')
+for i=1:1:loop_row
+    for j=1:1:loop_column
+        block = I(i:i+7,j:j+7);
+        DCT_block = dct2(block);
+        DCT_block = abs(DCT_block);
+        [x,y] = find(DCT_block==max(DCT_block(:))); % Find the largest coefficient
+        DCT_block(x,y) = -1; % Set the largest value as -Inf
+        [x,y] = find(DCT_block==max(DCT_block(:))); % Find the second largest coefficient and its position
+        feature = position_ref(x,y) + 1;
+        %Decide the binary mask
+        %Before decide the mask, we should caluate two class-conditionals
+        P_FG_Decision = P_x_FG(1,feature) * P_FG / (P_x_FG(1,feature)* P_FG + P_x_BG(1,feature) * P_BG);
+        P_BG_Decision = P_x_BG(1,feature) * P_BG / (P_x_FG(1,feature)* P_FG + P_x_BG(1,feature) * P_BG);
+        if P_FG_Decision >= P_BG_Decision
+            mask(i,j) = 1;
+        end
+    end
+end
+subplot(1,2,1)
+I = imread('dataset/cheetah_mask.bmp');
+imshow(I);
+subplot(1,2,2)
+imshow(mask);
+set(gcf,'Position',[400,100,900,600]);
+saveas(gcf, ['Images/segmentation.jpg']);
+close(gcf);
