f158ab49ab035dbccfec0c2f700bc1b539f71248
sniper-LAI <445124369@qq.com>
2019-10-20 01:05:59 -0700

    Finished 64 feature and mask

--- HW2/HW2_solution.m
+++ HW2/HW2_solution.m
@@ -7,62 +7,57 @@ load('dataset/TrainingSamplesDCT_8.mat');
 train_BG = TrainsampleDCT_BG;
 train_FG = TrainsampleDCT_FG;
 
-%Find the second largest value in each row of matrix train_BG
-[M_BG,N_BG] = max(train_BG,[],2);
-train_BG(bsxfun(@eq, train_BG, M_BG)) = -1; % Set the largest value in each row to -inf
-[M_BG,N_BG] = max(train_BG,[],2);
-
-%Find the second largest value in each row of matrix train_FG
-[M_FG,N_FG] = max(train_FG,[],2);
-train_FG(bsxfun(@eq, train_FG, M_FG)) = -1; % Set the largest value in each row to -inF
-[M_FG,N_FG] = max(train_FG,[],2);
-
-%Plot the frequency histogram
-subplot(2,1,1);
-h1 = histogram(N_BG);
-ylim([0, 500]); 
-ylabel('Frequency', 'interpreter', 'latex','FontSize', 10);
-xlabel('Index', 'interpreter', 'latex'); 
-title({['Frequency histograms of background data']},'Fontsize',12,'interpreter','latex');
-subplot(2,1,2);
-h2 = histogram(N_FG);
-ylim([0, 50]);
-ylabel('Frequency', 'interpreter', 'latex','FontSize', 10);
-xlabel('Index', 'interpreter', 'latex'); 
-title({['Frequency histograms of foreground data']},'Fontsize',12,'interpreter','latex');
-%Save the statistic data
-F_x_BG = zeros(1,64);
-F_x_BG(min(N_BG):max(N_BG)) = h1.Values;
-F_x_FG = zeros(1,64);
-F_x_FG(min(N_FG):max(N_FG)) = h2.Values;
-%Save the histogram figure
-set(gcf,'Position',[400,100,900,600]);
-saveas(gcf, ['Images/histograms1.jpg']);
-close(gcf);
-
-%Calculate the estimation of class-conditionals for two classes and priors probabilities
-P_x_BG = F_x_BG ./ sum(F_x_BG);
-P_x_FG = F_x_FG ./ sum(F_x_FG);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem (a)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 P_BG = size(train_BG,1) / (size(train_BG,1) + size(train_FG,1));
 P_FG = size(train_FG,1) / (size(train_BG,1) + size(train_FG,1));
 
-% %Plot the index histogram
-subplot(2,1,1);
-h1 = histogram(N_BG,'Normalization','pdf');
-ylim([0, 0.4]); 
-ylabel('Probability', 'interpreter', 'latex','FontSize', 10);
-xlabel('Index', 'interpreter', 'latex'); 
-title({['Index histograms of $$P_{X|Y}(x|grass)$$']},'Fontsize',12,'interpreter','latex');
-subplot(2,1,2);
-h2 = histogram(N_FG,'Normalization','pdf');
-ylim([0, 0.2]);
-ylabel('Probability', 'interpreter', 'latex','FontSize', 10);
-xlabel('Index', 'interpreter', 'latex'); 
-title({['Index histograms of $$P_{X|Y}(x|cheetah)$$']},'Fontsize',12,'interpreter','latex');
-%Save the histogram figure
-set(gcf,'Position',[400,100,900,600]);
-saveas(gcf, ['Images/histograms2.jpg']);
-close(gcf);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem (b)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Calculate the mean and variance of every features when cheetah
+%mean_ch is the mean and var_ch is the variance
+mean_ch = mean(train_FG,1);
+std_ch = std(train_FG,0,1);
+
+%Calculate the mean and variance of every features when grass
+%mean_gr is the mean and var_gr is the variance
+mean_gr = mean(train_BG,1);
+std_gr = std(train_BG,0,1);
+
+%Plot the 64-plots
+for j=1:1:4
+    position = 1; %Define the subplot row index
+    for i=(j-1)*16+1:1:(j-1)*16+16
+        figure(j);
+        subplot(4,4,position);
+        x=-(mean_ch(i)+4*std_ch(i)):0.0005:(mean_ch(i)+4*std_ch(i));
+        y=normpdf(x,mean_ch(i),std_ch(i));
+        %Plot the cheetah line
+        plot(x,y);
+        hold on
+        x=-(mean_gr(i)+4*std_gr(i)):0.0005:(mean_gr(i)+4*std_gr(i));
+        y=normpdf(x,mean_gr(i),std_gr(i));
+        %Plot the grass line and mark it as red
+        plot(x,y,'r');
+        position = position + 1;
+        title({['k=',num2str(i)]},'Fontsize',12,'interpreter','latex');
+    end
+    set(gcf,'Position',[400,100,900,600]);
+    %Save the images
+    saveas(gcf, ['Images/subplot',num2str(j),'.jpg']);
+    close(gcf);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem (c)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%The 64-dimensional Gaussians
+%Calculate the covariance matrix of cheetah
+cov_ch = cov(train_FG);
+%Calculate the covariance matrix of grass
+cov_gr = cov(train_BG);
 
 %Read original image
 I = imread('dataset/cheetah.bmp');
@@ -70,39 +65,30 @@ I = im2double(I);
 %Define the loop numbers
 loop_row = size(I,1) - 8 + 1;
 loop_column = size(I,2) - 8 + 1;
+%Caculate the threshold
+T = P_BG / P_FG;
 
 mask = zeros(size(I));
-position_ref = load('dataset/Zig-Zag Pattern.txt')
+%Read the Zig-Zag file
+position_ref = load('dataset/Zig-Zag Pattern.txt');
+%Define the array for saving DCT coeffiences according to Zig-Zag
+DCT_coeffience = zeros([1,64]);
+
 for i=1:1:loop_row
     for j=1:1:loop_column
         block = I(i:i+7,j:j+7);
         DCT_block = dct2(block);
-        DCT_block = abs(DCT_block);
-        [x,y] = find(DCT_block==max(DCT_block(:))); % Find the largest coefficient
-        DCT_block(x,y) = -1; % Set the largest value as -1
-        [x,y] = find(DCT_block==max(DCT_block(:))); % Find the second largest coefficient and its position
-        feature = position_ref(x,y) + 1;
-        %Decide the binary mask
-        %Before decide the mask, we should caluate two class-conditionals
-        P_FG_Decision = P_x_FG(1,feature) * P_FG / (P_x_FG(1,feature)* P_FG + P_x_BG(1,feature) * P_BG);
-        P_BG_Decision = P_x_BG(1,feature) * P_BG / (P_x_FG(1,feature)* P_FG + P_x_BG(1,feature) * P_BG);
-%         P_FG_Decision = P_x_FG(1,feature) / (P_x_FG(1,feature)* P_FG + P_x_BG(1,feature) * P_BG);
-%         P_BG_Decision = P_x_BG(1,feature) / (P_x_FG(1,feature)* P_FG + P_x_BG(1,feature) * P_BG);
-        if P_FG_Decision >= P_BG_Decision
+        %Map DCT_block matrix to array according Zig-Zag
+        for row=1:1:8
+            for column=1:1:8
+                DCT_coeffience(1,position_ref(row,column)+1)=DCT_block(row,column);
+            end
+        end
+        P_x_FG = mvnpdf(DCT_coeffience,mean_ch,cov_ch);
+        P_x_BG = mvnpdf(DCT_coeffience,mean_gr,cov_gr);
+        if P_x_FG/P_x_BG > T
             mask(i,j) = 1;
         end
     end
 end
-
-subplot(1,2,1)
-I = imread('dataset/cheetah_mask.bmp');
-I = im2double(I);
-imshow(I);
-subplot(1,2,2)
 imshow(mask);
-%Calculate the probability of error
-error = length(find((mask-I)~=0)) / (size(I,1) * size(I,2));
-title({['Probability of error is ',num2str(error*100,'%.2f'),'\%']},'Fontsize',12,'interpreter','latex');
-%set(gcf,'Position',[900,600]);
-saveas(gcf, ['Images/segmentation.jpg']);
-close(gcf);
