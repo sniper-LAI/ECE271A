fd3ade1795bd09838e1c08bbc6999e0018a56348
sniper-LAI <445124369@qq.com>
2019-11-30 15:39:00 -0800

    Add HW5 solution file

---
+++ HW5/HW5_solution.m
@@ -0,0 +1,8 @@
+clear all
+%Training
+%Read the TrainingSamplesDCT_8.mat file
+load('dataset/TrainingSamplesDCT_8_new.mat');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem a
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
