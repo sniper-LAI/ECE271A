c6c6c91ce60b37550ffce1b2e2ab9f336bc7e450
sniper-LAI <445124369@qq.com>
2019-11-30 22:06:54 -0800

    Finish parameter init function

--- HW5/HW5_solution.m
+++ HW5/HW5_solution.m
@@ -2,7 +2,12 @@ clear all
 %Training
 %Read the TrainingSamplesDCT_8.mat file
 load('dataset/TrainingSamplesDCT_8_new.mat');
+%Save TrainsampleDCT_BG and TrainsampleDCT_FG in temporary value
+train_BG = TrainsampleDCT_BG;
+train_FG = TrainsampleDCT_FG;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Problem a
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+C = 8;
+fun_EM(C, train_BG, train_FG);
