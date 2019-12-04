0157ce63564431660cd2376e63392c6c3670080c
sniper-LAI <445124369@qq.com>
2019-12-04 01:30:48 -0800

    Change local NO PUSH

--- HW5/myfunction/fun_BDR.m
+++ HW5/myfunction/fun_BDR.m
@@ -10,14 +10,17 @@ dimension = [1,2,4,8,16,24,32,40,48,56,64];
 C = length(weight_BG);
 error = [];
 for d=dimension
-    DCT_coeffience_selected = DCT_coeffience(:, 1:d);
-    mu_FG_selected = mu_FG(:, 1:d); 
-    mu_BG_selected = mu_BG(:, 1:d);
-    sigma_FG_selected = reshape(repmat(eye(d), [1, C])*diag(reshape(sigma_FG(:, 1:d)', [C*d, 1])), [d, d, C]);
-    sigma_BG_selected = reshape(repmat(eye(d), [1, C])*diag(reshape(sigma_BG(:, 1:d)', [C*d, 1])), [d, d, C]);
-    gm_FG = gmdistribution(mu_FG_selected,sigma_FG_selected,weight_FG);
-    gm_BG = gmdistribution(mu_BG_selected,sigma_BG_selected,weight_BG);
-    I_pre = pdf(gm_FG, DCT_coeffience_selected) > T * pdf(gm_BG, DCT_coeffience_selected);
+    DCT_coeffience_d = DCT_coeffience(:, 1:d);
+    % Select the corresponding parameters
+    mu_FG_d = mu_FG(:, 1:d); 
+    mu_BG_d = mu_BG(:, 1:d);
+    sigma_FG_d = reshape(repmat(eye(d), [1, C])*diag(reshape(sigma_FG(:, 1:d)', [C*d, 1])), [d, d, C]);
+    sigma_BG_d = reshape(repmat(eye(d), [1, C])*diag(reshape(sigma_BG(:, 1:d)', [C*d, 1])), [d, d, C]);
+    % Compute a predict mask
+    MixG_FG = gmdistribution(mu_FG_d,sigma_FG_d,weight_FG);
+    MixG_BG = gmdistribution(mu_BG_d,sigma_BG_d,weight_BG);
+    I_pre = pdf(MixG_FG, DCT_coeffience_d) > T * pdf(MixG_BG, DCT_coeffience_d);
+    % Compute PoE
     error_temp = fun_error(I_pre);
     error = [error,error_temp];
 end
